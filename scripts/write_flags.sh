2958b7dd695d92a6f913193eedecc3bea6e3d025
joepasss@gentoo <farwes1180@gmail.com>
2025-06-17 02:40:54 +0900

    change makeopts

--- scripts/write_flags.sh
+++ scripts/write_flags.sh
@@ -2,8 +2,9 @@
 
 set -euo pipefail
 
+# in prod, change to MAKEOPTS=-j1 (or 2)
 {
-  echo 'MAKEOPTS="-j1"'
+  echo "MAKEOPTS=\"-j$(nproc)\""
   echo 'USE="minimal seccomp -doc -examples -test -alsa -jpeg -pipewire -pulseaudio -jack -X -wayland -vnc -opengl -vulkan -cuda -bluetooth -apparmor -hardened -selinux -systemd -filecaps -jemalloc -lzo -snappy -nls -pam -sasl -smartcard -udev -oss -dbus -idn -lua"'
   echo 'QEMU_USER_TARGETS="i386 x86_64"'
   echo 'QEMU_SOFTMMU_TARGETS="i386 x86_64"'
