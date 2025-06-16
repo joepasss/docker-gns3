b42772f7e20c402345eeb42f4fff406d56452a06
joepasss <farwes1180@gmail.com>
2025-06-16 22:32:20 +0900

    change job=1

--- scripts/write_flags.sh
+++ scripts/write_flags.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-echo "MAKEOPTS=\"-j$(nproc)\"" >>/etc/portage/make.conf
+echo "MAKEOPTS=\"-j1\"" >>/etc/portage/make.conf
 echo "USE=\"minimal -doc -examples -test -alsa -jpeg -pipewire -png -pulseaudio -X -wayland -vnc -jack -opengl -vulkan -bluetooth\"" >>/etc/portage/make.conf
 
 echo "net-dns/dnsmasq script" >>/etc/portage/package.use/dnsmasq
